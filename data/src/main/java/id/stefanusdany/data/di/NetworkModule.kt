8e52c0f9d5a05b3427a7935b16d1a222495f662a
Dany <stefanus.dany@mekari.com>
2022-08-02 14:22:45 +0700

    implement Certificate Pinning

--- data/src/main/java/id/stefanusdany/data/di/NetworkModule.kt
+++ data/src/main/java/id/stefanusdany/data/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import id.stefanusdany.data.data.remote.retrofit.ApiService
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -18,10 +19,16 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add("story-api.dicoding.dev", "sha256/e4cHM4/Zl9x6ux46xTXuSPEgZKigz9Yv+voi3JEq71M=")
+            .add("story-api.dicoding.dev", "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+            .add("story-api.dicoding.dev", "sha256/C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
